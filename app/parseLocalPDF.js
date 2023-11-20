422cbc6abced1c113f71c60156c6b141f064d8d9
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-11-20 15:32:39 +0000

    scrape pdf and convert to excel, data manipulation incomplete

--- app/parseLocalPDF.js
+++ app/parseLocalPDF.js
@@ -1,27 +1,53 @@
-import PDFParser from 'pdf2json';
+const fs = require('fs');
+const pdf = require('pdf-parse');
+const XLSX = require('xlsx');
 
-async function parseLocalPDF() {
-  const pdfParser = new PDFParser();
-  const pdfPath = '/Users/eazzopardi/code/pdf-to-db/sample report card.pdf';
+async function parseLocalPDF(pdfPath) {
+  const dataBuffer = fs.readFileSync(pdfPath);
+  const data = await pdf(dataBuffer);
 
-  pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
-
-  pdfParser.on("pdfParser_dataReady", pdfData => {
-    console.log("PDF DATA", pdfData)
-      pdfData.Pages.forEach(page => {
-        page.Texts.forEach(text => {
-          const textContent = text.R.map(r => decodeURIComponent(r.T)).join(' ');
-          console.log(textContent);
-        });
-      });
-  });
-  
-  pdfParser.loadPDF(pdfPath);
+  const structuredData = parsePDFText(data.text);
+  const excelData = createExcelData(structuredData);
+  exportToExcel(excelData, 'output.xlsx');
 }
 
-parseLocalPDF();
+function parsePDFText(pdfText) {
+  const lines = pdfText.split('\n');
+  const data = {};
+  let lastKey = null;
+
+  lines.forEach(line => {
+    if (isKey(line)) {
+      lastKey = line.trim(); // Update the last key
+      data[lastKey] = null;  // Initialize the key with a null value
+    } else if (isValue(line) && lastKey) {
+      data[lastKey] = line.trim();
+    }
+  });
+
+  return data;
+}
+
+function isKey(line) {
+  return line.match(/[a-zA-Z]{3,}/); // Matches strings with more than 2 letters
+}
+
+function isValue(line) {
+  return !isNaN(line.trim()) && line.trim() !== ''; // Checks if the line is a number
+}
 
 
+function createExcelData(data) {
+  return [data]; // Wrapping the single object in an array to create a single row
+}
 
+function exportToExcel(data, fileName) {
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+  XLSX.writeFile(workbook, fileName);
+}
 
-
+// Replace with your actual PDF path
+const pdfPath = '/Users/eazzopardi/code/pdf-to-db/sample report card.pdf';
+parseLocalPDF(pdfPath);

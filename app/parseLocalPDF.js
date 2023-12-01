f62567bb9550b8e69e6c1d504fa68f46c2d5b955
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-01 15:47:35 +0000

    create array of known keys and use this to search for respective values

--- app/parseLocalPDF.js
+++ app/parseLocalPDF.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const { split } = require('postcss/lib/list');
 
 async function parseLocalPDF(pdfPath) {
   const dataBuffer = fs.readFileSync(pdfPath);
@@ -18,36 +19,58 @@ async function parseLocalPDF(pdfPath) {
 
 function parsePDFText(pdfText) {  
   const words = pdfText.split('\n');
-  for (let i = 0; i < words.length; i++) {
-    if (words[i] == 'State') {
-      if (words[i + 1] != 'District') {
-        console.log('State: ' + words[i + 1]);
-      } else {
-        console.log('no response');
-      }
-      break; // Break here, after checking the condition
+  const splitwords = []
+  words.forEach(word => {
+    if (/[a-z][A-Z]/.test(word)){
+    word = word.replace(/([a-z])([A-Z])/g, '$1 $2')
+    }
+    splitwords.push(word)
+  })
+  console.log("SPLIT WORDS", splitwords)
+  
+
+  arr = ['State', 'District', 'Block', 'Rural / Urban', 'Cluster', 'Ward', 'Pincode', 'Panchayat']
+    
+
+  for (let i = 0; i < splitwords.length; i++) {
+    if (arr.some(word => splitwords.includes(word)) && !arr.some(a => splitwords[i + 1] && splitwords[i + 1].includes(a))) {
+      console.log(splitwords[i] + ': ' + splitwords[i+1])
+       // Break here, after checking the condition
     }
   }
-  for (let i = 0; i < words.length; i++) {
-    if (words[i] == 'District') {
-      if (words[i + 1] != 'Block') {
-        console.log('District: ' + words[i + 1]);
-      } else {
-        console.log('no response');
-      }
-      break; // Break here, after checking the condition
-    }
-  }
-  for (let i = 0; i < words.length; i++) {
-    if (words[i] == 'Block') {
-      if (words[i + 1] != 'Rural / Urban') {
-        console.log('Block: ' + words[i + 1]);
-      } else {
-        console.log('no response');
-      }
-      break; // Break here, after checking the condition
-    }
-  }
+  // for (let i = 0; i < words.length; i++) {
+  //   if (words[i] == 'District') {
+  //     if (words[i + 1] != 'Block') {
+  //       console.log('District: ' + words[i + 1]);
+  //     } else {
+  //       console.log('no response');
+  //     }
+  //     break; // Break here, after checking the condition
+  //   }
+  // }
+  // for (let i = 0; i < words.length; i++) {
+  //   if (words[i] == 'Block') {
+  //     if (words[i + 1] != 'Rural / Urban') {
+  //       console.log('Block: ' + words[i + 1]);
+  //     } else {
+  //       console.log('no response');
+  //     }
+  //     break; // Break here, after checking the condition
+  //   }
+  // }
+  ////
+  // for (let i = 0; i < words.length; i++) {
+  //   if (words[i] == 'MohallaPincode') {
+  //     if (words[i + 1] != 'Pincode') {
+  //       console.log('Mohalla: ' + words[i + 1]);
+  //     } else {
+  //       console.log('no response');
+  //     }
+  //     break; // Break here, after checking the condition
+  //   }
+//   }
+// }
+// }
 }
 
 

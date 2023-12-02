83a95db6043602e630f3fe9cfe4726d53938ff1a
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-12-02 10:08:01 +0000

    variable and values matching

--- app/parseLocalPDF.js
+++ app/parseLocalPDF.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
-const { split } = require('postcss/lib/list');
+
 
 async function parseLocalPDF(pdfPath) {
   const dataBuffer = fs.readFileSync(pdfPath);
@@ -13,30 +13,35 @@ async function parseLocalPDF(pdfPath) {
   const data = await pdf(dataBuffer, options);
 
   parsePDFText(data.text);
-  // console.log(data)
+
 
 }
 
 function parsePDFText(pdfText) {  
   const words = pdfText.split('\n');
   
-  arr = ['State', 'District', 'Block', 'Rural / Urban', 'Cluster', 'Ward', 'Pincode', 'Panchayat']
+  arr = ['State', 'District', 'Block', 'Rural / Urban', 'Cluster', 'Ward', 'Pincode', 'Panchayat', 'City', 'Municipality', 'School Category', 'School Management', 'Medium 1']
     
 
-  for (let i = 0; i < words.length; i++) {
-    // check that words[i] matches a word in arr - words[i] concatenates some words so it is possible that the word in arr is a substring of a word in words
-    (arr.some(word => {
-      words.includes(word)
-      console.log(word)
-    }
-    )) 
+  words.forEach( (word, i) => {
+      // identify the variables
+      // check that the word matches a string in arr - words concatenates some strings so it is possible that the string in arr is a substring of a string in words
+       if (arr.some(variable=> word.includes(variable)))
+       {
+          // identify values
+          // Error handling needed here. Eg check that the following word, words[i + 1], exists in words but not in arr, that will check it is not a key but is indeed a value
+          const value = words[i + 1]
+          // console.log variable: value
+          console.log(word, " : ", value)
+       }
+      }
+  )
 
 
-    // check that the following word, words[i + 1], exists in words but not in arr, that will check it is not a key but is indeed a value
    
   
   }
-}
+
 
 
 const pdfPath = '/Users/eazzopardi/code/pdf-to-db/sample report card.pdf';
